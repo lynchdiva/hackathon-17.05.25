eb5b254a1ccf3fb71cd9bc5e744d0a92211336d8
001Smiling <98030501+001Smiling@users.noreply.github.com>
2025-05-17 20:16:11 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,4 @@
 
-import { updateDateTime, initHeaderEvents } from './header.js';
-
 document.addEventListener('DOMContentLoaded', () => {
     updateDateTime();
     setInterval(updateDateTime, 1000);
